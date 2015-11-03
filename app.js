147fcb42166d06a0712eb63b96f5859deb4b5be3
Andrea Gabriele <Andrea Gabriele>
2015-11-03 21:01:27 +0100

    prova

--- app.js
+++ app.js
@@ -5,9 +5,45 @@
 // done: when the user has discovered the pair of cards
 // hidden: when the card has not been discovered yet
 
+
+var template= '<li id="idn" class="classname">' +
+              "<h3>number</h3>" 
+
 $(document).ready(function(){
 
-  
+   for (var i=0; i<data.length; i++){
+            $(".cards").append(template.replace("classname", "done" )
+                                       .replace("idn", data[i] )
+                                       .replace("number", data[i]))};
+                               
+    
+    
+    $(".opt-start").click(function(){
+      $("li").addClass('hidden').removeClass('done');
+        
+    });
+    
+    
+    var conteggio=1;
+    $("li").click(function(){
+       if(this.id==conteggio){
+          $(this).addClass('done').removeClass('hidden');
+          conteggio++;
+       }else{
+          alert("Hai perso");
+          $("li").addClass('hidden').removeClass('done');
+          conteggio=1;
+       }
+     if(conteggio==11){
+        alert("Hai vinto!");
+     }  
+    });
+    
+    
+    
+    
+    
+    
 });
 
 
